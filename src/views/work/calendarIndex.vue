c02baf8e7c16da6c6801edc5227429ace6f0049b
j92z <zeco11320@163.com>
2021-04-01 16:54:33 +0800

    fix

--- src/views/work/calendarIndex.vue
+++ src/views/work/calendarIndex.vue
@@ -118,8 +118,9 @@
         </el-container>
       </el-main>
     </el-container>
-    <el-aside :width="asideShow ? '30%' : '0px'" class="aside-table">
+    <el-aside v-if="asideShow" width="30%" class="aside-table">
       <template>
+        <el-button type="plain" icon="el-icon-plus" @click="taggleWorkDrawer(true)">添加任务</el-button>
         <el-table :data="asideData" style="width: 100%">
           <el-table-column label="名称">
             <template slot-scope="scope">
@@ -139,8 +140,14 @@
           <el-table-column label="状态" width="80">
             <template slot-scope="scope">
               <el-tag v-if="scope.row.status === 1" type="success">完成</el-tag>
-              <el-tag v-else-if="scope.row.status === 0" type="info">未完成</el-tag>
-              <el-tag v-else-if="scope.row.status === -1" type="danger">失败</el-tag>
+              <el-tag
+                v-else-if="scope.row.status === 0"
+                type="info"
+              >未完成</el-tag>
+              <el-tag
+                v-else-if="scope.row.status === -1"
+                type="danger"
+              >失败</el-tag>
             </template>
           </el-table-column>
           <el-table-column label="操作" width="60">
@@ -151,8 +158,17 @@
                 @click="workItemDone(scope.row.id)"
               >完成</el-button> -->
               <span style="font-size: 30px">
-                <i v-if="scope.row.status !== 1" class="el-icon-check" style="color: green;cursor: pointer" @click="workItemDone(scope.row.id)" />
-                <i v-else class="el-icon-check" style="color: gray;cursor: no-drop" />
+                <i
+                  v-if="scope.row.status !== 1"
+                  class="el-icon-check"
+                  style="color: green; cursor: pointer"
+                  @click="workItemDone(scope.row.id)"
+                />
+                <i
+                  v-else
+                  class="el-icon-check"
+                  style="color: gray; cursor: no-drop"
+                />
               </span>
               <!-- <el-button
                 size="mini"
@@ -165,14 +181,102 @@
         </el-table>
       </template>
     </el-aside>
+    <template>
+      <el-drawer
+        direction="rtl"
+        destroy-on-close
+        :visible.sync="workDrawer"
+        :with-header="false"
+      >
+        <template>
+          <el-form
+            ref="workForm"
+            :model="workForm"
+            label-width="80px"
+            style="padding: 20px"
+          >
+            <el-form-item
+              label="任务名称"
+              :rules="[{ required: true, message: '任务名称不可为空' }]"
+              prop="name"
+            >
+              <el-input v-model="workForm.name" />
+            </el-form-item>
+            <el-form-item
+              label="任务内容"
+              :rules="[{ required: true, message: '任务名称不可为空' }]"
+              prop="content"
+            >
+              <el-input
+                v-model="workForm.content"
+                type="textarea"
+                :autosize="{ minRows: 2 }"
+              />
+            </el-form-item>
+            <el-form-item
+              label="工作日期"
+              :rules="[{ required: true, message: '工作日期不可为空' }]"
+            >
+              <el-date-picker
+                v-model="workForm.date"
+                type="date"
+                placeholder="选择日期"
+              />
+            </el-form-item>
+            <el-form-item
+              label="工作时间"
+              :rules="[{ required: true, message: '工作时间不可为空' }]"
+              prop="timeRange"
+            >
+              <template>
+                <el-time-picker
+                  v-model="workForm.timeRange"
+                  is-range
+                  range-separator="至"
+                  start-placeholder="开始时间"
+                  end-placeholder="结束时间"
+                  placeholder="选择时间范围"
+                />
+              </template>
+            </el-form-item>
+            <el-form-item label="任务排序">
+              <el-input-number
+                v-model="workForm.sort"
+                :step="1"
+                step-strictly
+                :max="1000"
+              />
+            </el-form-item>
+            <el-form-item label="归属计划">
+              <el-cascader
+                v-model="workForm.planCascaderPath"
+                :options="treeList"
+                :props="{ checkStrictly: true, label: 'name', value: 'id' }"
+                clearable
+                :show-all-levels="false"
+              />
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                :loading="addLoading"
+                type="primary"
+                @click="addOneWork"
+              >立即创建</el-button>
+              <el-button @click="taggleWorkDrawer(false)">取消</el-button>
+            </el-form-item>
+          </el-form>
+        </template>
+      </el-drawer>
+    </template>
   </el-container>
 </template>
 
 <script>
-import { workCalendar, doneWorkItem, failWorkItem } from '@/api/work'
+import { workCalendar, doneWorkItem, failWorkItem, createOneWork } from '@/api/work'
 // import calendarItem from './components/calendarItem'
 import { getDateList } from '@/utils/date/calendar'
 import { parseTime } from '@/utils/index'
+import { getTree } from '@/api/plan'
 
 export default {
   // components: {
@@ -190,7 +294,18 @@ export default {
       dateList: [],
       height: 200,
       asideData: [],
-      asideShow: false
+      asideShow: false,
+      treeList: [],
+      workDrawer: false,
+      addLoading: false,
+      workForm: {
+        name: '',
+        content: '',
+        timeRange: '',
+        date: '',
+        sort: 0,
+        planCascaderPath: []
+      }
     }
   },
   created() {
@@ -209,6 +324,11 @@ export default {
         if (setAside) {
           this.asideData = this.calendarList[this.selectDate]
         }
+      })
+    },
+    fetchPlanList() {
+      getTree().then((response) => {
+        this.treeList = response.data
       })
     },
     formatWeek(start = 0) {
@@ -282,13 +402,28 @@ export default {
       }
     },
     setSelcetDate(date = new Date(), init = false) {
-      const newSelectDate = date.getFullYear().toString() + '-' + (date.getMonth() < 10 ? '0' : '') + (date.getMonth() + 1).toString() + '-' + (date.getDate() < 10 ? '0' : '') + date.getDate().toString()
-      init || this.taggleAside(newSelectDate !== this.selectDate || (newSelectDate === this.selectDate && this.asideShow === false), newSelectDate)
+      const newSelectDate =
+        date.getFullYear().toString() +
+        '-' +
+        (date.getMonth() < 10 ? '0' : '') +
+        (date.getMonth() + 1).toString() +
+        '-' +
+        (date.getDate() < 10 ? '0' : '') +
+        date.getDate().toString()
+      init ||
+        this.taggleAside(
+          newSelectDate !== this.selectDate ||
+            (newSelectDate === this.selectDate && this.asideShow === false),
+          newSelectDate
+        )
       this.selectDate = newSelectDate
     },
     taggleAside(taggle = true, newSelectDate = null) {
       if (taggle && newSelectDate) {
-        this.asideData = this.calendarList[newSelectDate] !== undefined ? this.calendarList[newSelectDate] : []
+        this.asideData =
+          this.calendarList[newSelectDate] !== undefined
+            ? this.calendarList[newSelectDate]
+            : []
       }
       this.asideShow = taggle
     },
@@ -311,6 +446,60 @@ export default {
           type: 'success'
         })
         this.getCalendarList(true)
+      })
+    },
+    taggleWorkDrawer(taggle) {
+      if (taggle) {
+        if (this.treeList.length === 0) {
+          this.fetchPlanList()
+        }
+        this.workForm.date = new Date(this.selectDate.replace(/-/g, '/'))
+      } else {
+        this.$refs['workForm'].resetFields()
+      }
+      this.workDrawer = taggle
+    },
+    addOneWork() {
+      this.$refs['workForm'].validate((valid) => {
+        if (valid) {
+          this.addLoading = true
+          const workDate = this.workForm.date
+          workDate.setMinutes(
+            workDate.getMinutes() - workDate.getTimezoneOffset()
+          )
+          const dayWorkStartTime = this.workForm.timeRange[0]
+          const dayWorkEndTime = this.workForm.timeRange[1]
+          var info = {
+            name: this.workForm.name,
+            content: this.workForm.content,
+            date: workDate.toJSON().substr(0, 10),
+            dayWorkStartTime:
+              (dayWorkStartTime.getTime()) % 86400000,
+            dayWorkEndTime:
+              (dayWorkEndTime.getTime()) % 86400000,
+            sort: this.workForm.sort,
+            planId: this.id
+          }
+          createOneWork(info).then(() => {
+            this.$message({
+              message: '添加成功',
+              type: 'success'
+            })
+            this.workForm = {
+              name: '',
+              content: '',
+              timeRange: '',
+              date: '',
+              sort: 0,
+              planCascaderPath: []
+            }
+            this.$refs['workForm'].resetFields()
+            this.addLoading = false
+            this.taggleWorkDrawer(false)
+            this.getCalendarList(true)
+          })
+          this.addLoading = false
+        }
       })
     }
   }
